0634af4ff152babd12a974959836c8ca0d1d8331
Saamer Mansoor <i@saamer.me>
2019-03-19 04:10:42 -0400

    Enhanced KombuchaPage to get current status on load

--- WheresMyKombucha/WheresMyKombucha/KombuchaPage.cs
+++ WheresMyKombucha/WheresMyKombucha/KombuchaPage.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace WheresMyKombucha
 {
     public class KombuchaPage : ContentPage
     {
+        string AccessToken = "e54ec36b6b139319129d8cd075cb88f095a9dce7";
         Label _resultLabel = new Label()
         {
             HorizontalOptions = LayoutOptions.CenterAndExpand
         };
+
         public KombuchaPage()
         {
             var ChangeLedCommandOn = new Command<string>(LedOn);
@@ -20,7 +23,7 @@ namespace WheresMyKombucha
             Title = "Where's My Kombucha";
             Content = new StackLayout
             {
-                VerticalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 Children = {
                     new Label {
@@ -32,12 +35,14 @@ namespace WheresMyKombucha
                     _resultLabel
                 }
             };
+            GetKombuchaStatus();
         }
 
-        public void LedOn(string changeValue = "on")
+        #region Private Functions
+        private void LedOn(string changeValue = "on")
         {
             changeValue = "on";
-            string accessToken = "e54ec36b6b139319129d8cd075cb88f095a9dce7"; //This is your Particle Cloud Access Token
+            string accessToken = AccessToken; //This is your Particle Cloud Access Token
             string deviceId = "28003d001847343338333633"; //This is your Particle Device Id
             string partilceFunc = "led"; //This is the name of your Particle Function
 
@@ -56,11 +61,12 @@ namespace WheresMyKombucha
             var result = client.PostAsync("v1/devices/" + deviceId + "/" + partilceFunc, content);
             _resultLabel.TextColor = Color.Blue;
             _resultLabel.Text = "The Kombucha is here!";
+            ResultLabelUpdate(1);
         }
-        public void LedOff(string changeValue = "off")
+        private void LedOff(string changeValue = "off")
         {
             changeValue = "off";
-            string accessToken = "e54ec36b6b139319129d8cd075cb88f095a9dce7"; //This is your Particle Cloud Access Token
+            string accessToken = AccessToken; //This is your Particle Cloud Access Token
             string deviceId = "28003d001847343338333633"; //This is your Particle Device Id
             string partilceFunc = "led"; //This is the name of your Particle Function
 
@@ -77,9 +83,57 @@ namespace WheresMyKombucha
             });
 
             var result = client.PostAsync("v1/devices/" + deviceId + "/" + partilceFunc, content);
-            _resultLabel.TextColor = Color.Red;
-            _resultLabel.Text = "Rats! The Kombucha is gone again";
+            ResultLabelUpdate(0);
         }
+        private void ResultLabelUpdate(int status = -1)
+        {
+            if (status == 1)
+            {
+                _resultLabel.TextColor = Color.Blue;
+                _resultLabel.Text = "The Kombucha is here!";
+                BackgroundColor = Color.SkyBlue;
+            }
+            else if (status == 0)
+            {
+                _resultLabel.TextColor = Color.Red;
+                _resultLabel.Text = "Rats! The Kombucha is gone again";
+                BackgroundColor = Color.LightGray;
+            }
+            else
+            {
+                _resultLabel.Text = "Kombucha Status: Unknown";
+                BackgroundColor = Color.LightYellow;
+            }
+        }
+        private void GetKombuchaStatus()
+        {
+            string deviceId = "28003d001847343338333633"; //This is your Particle Device Id
+            string accessToken = AccessToken; //This is your Particle Cloud Access Token
+            HttpClient client = new HttpClient();
+            var url = string.Format("https://api.particle.io/v1/devices/{0}/digitalValue?access_token={1}", deviceId, AccessToken);
+            var json = client.GetStringAsync(url);
+            var result = JsonConvert.DeserializeObject<ParticleResponse>(json.Result);
+            ResultLabelUpdate(result.result);
+        }
+        #endregion
+    }
+
+    public class CoreInfo
+    {
+        public string last_app { get; set; }
+        public DateTime last_heard { get; set; }
+        public bool connected { get; set; }
+        public DateTime last_handshake_at { get; set; }
+        public string deviceID { get; set; }
+        public int product_id { get; set; }
+    }
+
+    public class ParticleResponse
+    {
+        public string cmd { get; set; }
+        public string name { get; set; }
+        public int result { get; set; }
+        public CoreInfo coreInfo { get; set; }
     }
 }
 
